db7940a66472d765722240a1e2e27a421a0c30a2
Tom Denley <tsdenley@gmail.com>
2016-10-05 13:43:07 +0100

    Init; simple free drawing canvas example

---
+++ main.js
@@ -0,0 +1,41 @@
+(function() {
+
+  var getID = function (id) { return document.getElementById(id) };
+
+  // Canvas
+  var canvas = getID('my_canvas'),
+    context = canvas.getContext('2d');
+    context.lineJoin = "round";
+    context.lineCap = "round";
+    context.lineWidth = 5;
+
+  // Clear canvas
+  var clearBtn = getID('clear').onclick = function () {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  };
+
+
+  /**
+  * Draw
+  */
+  var draw = function (e) {
+    var x = e.x - this.offsetLeft,
+      y = e.y - this.offsetTop;
+
+    context.lineTo(x, y);
+    context.stroke();
+  };
+
+  canvas.addEventListener('mousedown', function (e) {
+    var x = e.x - this.offsetLeft,
+      y = e.y - this.offsetTop;
+
+    context.moveTo(x, y);
+    canvas.addEventListener('mousemove', draw);
+  });
+
+  canvas.addEventListener('mouseup', function () {
+    canvas.removeEventListener('mousemove', draw);
+  });
+
+}());
